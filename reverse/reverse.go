24d8d6f5d3e5569a75fa8944861d786a8081672a
Surraiya Islam <i.surraiya@qa-academy.by>
2023-11-05 23:27:42 +0200

    Added encrypt and decrypt function in a same file

---
+++ reverse/reverse.go
@@ -0,0 +1,13 @@
+package reverse
+
+func Encrypt_reverse(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+func Decrypt_reverse(s string) string {
+	return Encrypt_reverse(s)
+}
