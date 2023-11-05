a6352ca270c68132d7c991c33cb79183ca96e6bf
Surraiya Islam <i.surraiya@qa-academy.by>
2023-11-06 00:01:08 +0200

    Modified logic

--- reverse/reverse.go
+++ reverse/reverse.go
@@ -1,11 +1,20 @@
 package reverse
 
 func Encrypt_reverse(s string) string {
-	runes := []rune(s)
-	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-		runes[i], runes[j] = runes[j], runes[i]
+	var reversed []rune
+	for _, char := range s {
+		if ('a' <= char && char <= 'z') || ('A' <= char && char <= 'Z') {
+			if 'a' <= char && char <= 'z' {
+				reversed = append(reversed, 'z'-(char-'a'))
+			} else {
+				reversed = append(reversed, 'Z'-(char-'A'))
+			}
+		} else {
+			reversed = append(reversed, char) // Non-alphabet characters are left unchanged
+		}
 	}
-	return string(runes)
+
+	return string(reversed)
 }
 
 func Decrypt_reverse(s string) string {
